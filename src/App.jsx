abefc451004bed0c69c8b2b73346141bdb4e7d11
Lenny Kay <77846195+lenbot7@users.noreply.github.com>
2024-06-29 12:29:36 +0000

    i was forced to change this

--- src/App.jsx
+++ src/App.jsx
@@ -24,17 +24,16 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Lenny Kay",
+  title: "Hobbyist Programmer",
+  email: "lennynoahkay@gmail.com",
+  gitHub: "lenbot7",
+  instagram: "lenbot7",
+  linkedIn: "lenny-kay",
+  medium: "lenbot7",
+  twitter: "lenbot7",
+  youTube: "lenbot7",
 };
-
 const primaryColor = "#4E567E";
 const secondaryColor = "#D2F1E4";
 
